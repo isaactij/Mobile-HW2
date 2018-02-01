3c1884b5e74de237b4a16024033b7dbdea60247f
Isaac <isaac@Isaacs-Mac.local>
2018-01-31 21:29:27 -0600

    Keyboard closes when save button is pressed and screen is touched anywhere other than the text fields. Added comments

--- TijerinaIsaac-hw2/TijerinaIsaac-hw2/ViewController.swift
+++ TijerinaIsaac-hw2/TijerinaIsaac-hw2/ViewController.swift
@@ -9,14 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate{
-    @IBOutlet weak var nameLabel: UITextField!
-    @IBOutlet weak var cityLabel: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var cityTextField: UITextField!
     @IBOutlet weak var messageLabel: UILabel!
     
+    //Runs when app initally opens
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Sets the message text to empty so the user does not see anything on inital run
         messageLabel.text = ""
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,26 +25,45 @@ class ViewController: UIViewController, UITextFieldDelegate{
         // Dispose of any resources that can be recreated.
     }
 
+    //Runs when the save button is clicked
     @IBAction func btnSaveClicked(_ sender: Any) {
-        guard let name = nameLabel.text else {
-            messageLabel.text = "You must enter a value for *both* name and city"
+        //Closes the key board
+        nameTextField.resignFirstResponder()
+        cityTextField.resignFirstResponder()
+        //Checks one at a time to see if the name and city text fields are unwrapable
+        //and if not it gives a error message and returns
+        guard var name = nameTextField.text else {
+            messageLabel.text = "You must enter a value for *both* name and city!!"
             return
         }
-        guard let city = cityLabel.text  else {
-            messageLabel.text = "You must enter a value for *both* name and city"
+        guard var city = cityTextField.text  else {
+            messageLabel.text = "You must enter a value for *both* name and city!!"
             return
         }
+        //Removes any trailing spaces
+        name = name.trimmingCharacters(in: .whitespaces)
+        city = city.trimmingCharacters(in: .whitespaces)
+        //Checks to see if name or city is empty and if so gives an error message
+        //If not it sets the message label to the name and city values
         if(name == "" || city == ""){
-            messageLabel.text = "You must enter a value for *both* name and city"
+            messageLabel.text = "You must enter a value for *both* name and city!!"
         }else{
             messageLabel.text = "\(name) - \(city)"
         }
     }
     
+    //Runs when "Done" is pressed on the keyboard
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        nameLabel.resignFirstResponder()
-        cityLabel.resignFirstResponder()
+        //Closes the keyboard
+        nameTextField.resignFirstResponder()
+        cityTextField.resignFirstResponder()
         return true
+    }
+    
+    //Runs when the screen is pressed anywhere except for  the text fields and save button
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //Closes the keyboard
+        self.view.endEditing(true)
     }
 }
 
